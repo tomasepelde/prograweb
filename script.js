79094abe3888774e4e169fb2551a4d184b204071
tomasepelde <tepeldecalcagno@itba.edu.ar>
2024-06-14 13:00:06 -0300

    cambios nuevos carrito

--- script.js
+++ script.js
@@ -21,6 +21,26 @@ const LIBROS = [
         "https://denomades.s3.us-west-2.amazonaws.com/blog/wp-content/uploads/2016/03/23111013/9788499185750.jpg"
 },
 
+{
+    id : 4,
+    nombre: "Maravillas",
+    precio: 17500,
+    img:
+        "https://img.remediosdigitales.com/017757/image/450_1000.jpeg"
+},{
+    id : 5,
+    nombre: "Saudade",
+    precio: 17500,
+    img:
+        "https://http2.mlstatic.com/D_NQ_NP_863421-MLU71096003034_082023-O.webp"
+},{
+    id : 6,
+    nombre: "Wonderlust",
+    precio: 17500,
+    img:
+        "https://images.cdn1.buscalibre.com/fit-in/360x360/73/4a/734a569dcc4f6b3a2d601a11bdc98437.jpg"
+}
+
 ];
 
 
@@ -44,13 +64,24 @@ LIBROS.forEach((libro)=> {
     
 
     botonComprar.addEventListener("click", () => {
+        const productoEnCarrito = CARRITO.find(item => item.id === libro.id);
+
+    if (productoEnCarrito) {
+        
+        productoEnCarrito.cantidad += 1;
+        productoEnCarrito.total = productoEnCarrito.cantidad * libro.precio; }
+    
+        else{
         CARRITO.push({
             id : libro.id,
             img : libro.img,
             nombre : libro.nombre,
             precio : libro.precio,
+            cantidad: 1,
+            total: libro.precio
 
-        });
+
+        });}
     });
 });
 
@@ -85,18 +116,62 @@ verCarrito.addEventListener("click", () => {
         contenidoCarrito.className = "contenido-carrito"
         contenidoCarrito.innerHTML = `
             <img src = "${libro.img}">
-            <h3>${libro.nombre}</h3>
-            <p> $ ${libro.precio} </p>
+            <h3>${libro.nombre}</h3> 
+            <h4> Cantidad:  ${libro.cantidad} </h4> 
+            <h4> Total: $ ${libro.total} </h4>
         `;
 
     carritoContainer.append(contenidoCarrito)
     });
 
-    const total = CARRITO.reduce((acumulador, producto) => acumulador + producto.precio,0);
+    const total = CARRITO.reduce((acumulador, producto) => acumulador + (producto.precio * producto.cantidad), 0);
 
     const totalCompra = document.createElement("div")
 
     totalCompra.className = "total-contenido"
     totalCompra.innerHTML = `Total a pagar: $ ${total}`;
-    carritoContainer.append(totalCompra)
-})
+    carritoContainer.append(totalCompra);
+
+    
+    const CompraFinal = document.createElement("button")
+
+    CompraFinal.className = "compra-final"
+    CompraFinal.innerHTML = `Comprar`;
+    carritoContainer.append(CompraFinal);
+
+
+    CompraFinal.addEventListener("click", () => {
+        if(CARRITO.length===0) {
+            Swal.fire({
+                title: "El carrito está vacío, volvé a intentarlo!",
+                width: 600,
+                padding: "3em",
+                color: "#716add",
+                backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("https://media.tenor.com/iyfN9RjdvR4AAAAM/panda-shy.gif")
+                    left top
+                    no-repeat
+                `})
+                carritoContainer.style.display = "none"; 
+        }
+        else {
+        Swal.fire({
+            title: "Compra realizada exitosamente!",
+            width: 600,
+            padding: "3em",
+            color: "#716add",
+            background: "#fff url(/images/trees.png)",
+            backdrop: `
+                rgba(0,0,123,0.4)
+                url("https://media2.giphy.com/media/MeOwyNnXJqXGXfsZ4m/200.webp?cid=790b7611f7kq4vukf3m4hwswmhza9wnzu45flmsqd5rfltye&ep=v1_gifs_search&rid=200.webp&ct=g")
+                left top
+                no-repeat
+            `
+        }); CARRITO = []; 
+        carritoContainer.style.display = "none"; 
+    }
+
+            })
+        
+        })
